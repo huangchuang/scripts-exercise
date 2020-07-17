8fc3b0e4082ab5a1a3fa038b121ac955009a536c
huangchuang <huangchuang@users.noreply.github.com>
2020-07-17 16:22:53 +0800

    Update

--- RemoteCommand.Example.ps1
+++ RemoteCommand.Example.ps1
@@ -5,29 +5,17 @@
 #     4. invoke-command -computername localhost -Scriptblock {Get-WmiObject -class win32_processor | select-object -ExpandProperty MaxClockSpeed | format-table HideTableHeaders | out-string}
 #     5. invoke-command -computername $Hostname -credential $Cred -scriptblock {Get-WmiObject -class win32_processor | select-object -ExpandProperty MaxClockSpeed | format-table HideTableHeaders | out-string}
 
-# working
+# working with domain accounts only on in-domain machines
 $Hostname = 'cnshg-server1'
 $Username = 'ap\huangcf'
-$Password = 'Doudou3@gtcc'
+$Password = 'Doudou12@gtcc'
 $pass = ConvertTo-SecureString -AsPlainText $Password -Force
 $Cred = New-Object System.Management.Automation.PSCredential -ArgumentList $Username,$pass
 invoke-command -computername $Hostname -credential $Cred -scriptblock {systeminfo}
 
-
-$Hostname = 'USTR-R3T27S22.ap.uis.unisys.com'
-$Username = 'USTR-R3T27S22\ABSuite'
-$Password = 'Unisys*2012'
-$pass = ConvertTo-SecureString -AsPlainText $Password -Force
-$Cred = New-Object System.Management.Automation.PSCredential -ArgumentList $Username,$pass
-invoke-command -computername $Hostname -credential $Cred -FilePath "D:\OneDrive - Unisys\GetMachineInfo.ps1" -ArgumentList {0}
-invoke-command -computername $Hostname -credential $Cred -scriptblock {Get-WmiObject -class win32_processor | select-object -ExpandProperty MaxClockSpeed | format-table HideTableHeaders | out-string}
-
-
-# Testing
-$Hostname = 'USTR-R2T9-27
-$Username = 'USTR-R2T9-27\ABSuite'
-$Password = 'Administer4Me'
+$Hostname = 'USTR-R1T10S23'
+$Username = 'ap\huangcf'
+$Password = 'Doudou12@gtcc'
 $pass = ConvertTo-SecureString -AsPlainText $Password -Force
 $Cred = New-Object System.Management.Automation.PSCredential -ArgumentList $Username,$pass
 invoke-command -computername $Hostname -credential $Cred -scriptblock {systeminfo}
-
--- RemoteCommand.ps1
+++ RemoteCommand.ps1
@@ -12,7 +12,7 @@ function connect ($Hostname, $Username, $Password, $FilePath)
 	invoke-command -computername $Hostname -credential $Cred -FilePath $FilePath
 }
 
-# RemoteCommand.ps1 -Hostname USTR-R3T27S22.ap.uis.unisys.com -Username USTR-R3T27S22\ABSuite -Password Unisys*2012 -FilePath "D:\huangchuang\scripts-exercise\GetMachineInfo.ps1"
+# RemoteCommand.ps1 -Hostname USTR-R3T27S22.ap.uis.unisys.com -Username USTR-R3T27S22\ABSuite -Password Unisys*1 -FilePath "D:\huangchuang\scripts-exercise\GetMachineInfo.ps1"
 # connect "USTR-R3T27S22.ap.uis.unisys.com" "USTR-R3T27S22\ABSuite" "Unisys*2012" "D:\huangchuang\scripts-exercise\GetMachineInfo.ps1"
 
 connect $Hostname $Username $Password $FilePath
